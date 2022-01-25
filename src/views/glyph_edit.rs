2154ea5c2fbf998aeb642431d695641d1c824735
Manos Pitsidianakis <el13635@mail.ntua.gr>
2022-01-25 14:34:22 +0200

    Add opening edit glyph widget, and exiting

--- src/views/glyph_edit.rs
+++ src/views/glyph_edit.rs
@@ -24,13 +24,18 @@ use gtk::glib;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use once_cell::unsync::OnceCell;
+use std::cell::Cell;
 
 use crate::project::Glyph;
 
 #[derive(Debug, Default)]
 pub struct GlyphEditArea {
+    app: OnceCell<gtk::Application>,
     glyph: OnceCell<Glyph>,
     drawing_area: OnceCell<gtk::DrawingArea>,
+    camera: Cell<(f64, f64)>,
+    mouse: Cell<(f64, f64)>,
+    button: Cell<Option<u32>>,
 }
 
 #[glib::object_subclass]
@@ -46,63 +51,103 @@ impl ObjectImpl for GlyphEditArea {
     // and where we can initialize things.
     fn constructed(&self, obj: &Self::Type) {
         self.parent_constructed(obj);
+        self.camera.set((0., 0.));
+        self.mouse.set((0., 0.));
 
         let drawing_area = gtk::DrawingArea::builder()
             .expand(true)
             .visible(true)
             .build();
+        drawing_area.set_events(
+            gtk::gdk::EventMask::BUTTON_PRESS_MASK
+                | gtk::gdk::EventMask::BUTTON_RELEASE_MASK
+                | gtk::gdk::EventMask::POINTER_MOTION_MASK,
+        );
+        drawing_area.connect_button_press_event(
+            clone!(@weak obj => @default-return Inhibit(false), move |_self, event| {
+
+                obj.imp().mouse.set(event.position());
+                obj.imp().button.set(Some(event.button()));
+
+                Inhibit(false)
+            }),
+        );
+        drawing_area.connect_button_release_event(
+            clone!(@weak obj => @default-return Inhibit(false), move |_self, event| {
+                obj.imp().mouse.set((0., 0.));
+                obj.imp().button.set(None);
+                    if let Some(screen) = _self.window() {
+                        let display = screen.display();
+                        screen.set_cursor(Some(
+                                &gtk::gdk::Cursor::from_name(&display, "default").unwrap(),
+                        ));
+                    }
+
+                Inhibit(false)
+            }),
+        );
+        drawing_area.connect_motion_notify_event(
+            clone!(@weak obj => @default-return Inhibit(false), move |_self, event| {
+                if let Some(gtk::gdk::BUTTON_SECONDARY) = obj.imp().button.get(){
+                    let mut camera = obj.imp().camera.get();
+                    let mouse = obj.imp().mouse.get();
+                    camera.0 += event.position().0 - mouse.0;
+                    camera.1 += event.position().1 - mouse.1;
+                    obj.imp().camera.set(camera);
+                    obj.imp().mouse.set(event.position());
+                    if let Some(screen) = _self.window() {
+                        let display = screen.display();
+                        screen.set_cursor(Some(
+                                &gtk::gdk::Cursor::from_name(&display, "grab").unwrap(),
+                        ));
+                    }
+                    _self.queue_draw()
+                }
+
+                Inhibit(false)
+            }),
+        );
+
         drawing_area.connect_draw(clone!(@weak obj => @default-return Inhibit(false), move |drar: &gtk::DrawingArea, cr: &gtk::cairo::Context| {
-            cr.scale(500f64, 500f64);
+            let width = drar.allocated_width() as f64;
+            let height = drar.allocated_height() as f64;
             cr.set_source_rgb(1., 1., 1.);
             cr.paint().expect("Invalid cairo surface state");
-            cr.set_source_rgba(0.0, 0.0, 0.0, 0.5);
-            cr.set_line_width(0.0005);
-            for x in (0..100).into_iter().step_by(5) {
-                let x = x as f64 / 100.;
-                cr.move_to(x, 0.);
-                cr.line_to(x, 1.);
-                cr.stroke().expect("Invalid cairo surface state");
+
+            let camera = obj.imp().camera.get();
+            cr.set_line_width(1.0);
+            for &(color, step) in &[(0.8, 5.0), (0.2, 100.0)] {
+                cr.set_source_rgb(color, color, color);
+                let mut y = (camera.1 % step).floor() + 0.5;
+                while y < height {
+                    cr.move_to(0., y);
+                    cr.line_to(width, y);
+                    y += step;
+                }
+                cr.stroke().unwrap();
+                let mut x = (camera.0 % step).floor() + 0.5;
+                while x < width {
+                    cr.move_to(x, 0.);
+                    cr.line_to(x, height);
+                    x += step;
+                }
+                cr.stroke().unwrap();
             }
-            for y in (0..100).into_iter().step_by(5) {
-                let y = y as f64 / 100.;
-                cr.move_to(0., y);
-                cr.line_to(1., y);
-                cr.stroke().expect("Invalid cairo surface state");
-            }
-            /*
-            cr.set_source_rgb(0., 0., 0.);
-
-            cr.select_font_face("Sans", FontSlant::Normal, FontWeight::Normal);
-            cr.set_font_size(0.35);
-
-            cr.move_to(0.04, 0.53);
-            cr.show_text("Hello").expect("Invalid cairo surface state");
-
-            cr.move_to(0.27, 0.65);
-            cr.text_path("void");
-            cr.set_source_rgb(0.5, 0.5, 1.0);
-            cr.fill_preserve().expect("Invalid cairo surface state");
-            cr.set_source_rgb(0.0, 0.0, 0.0);
-            cr.set_line_width(0.01);
-            cr.stroke().expect("Invalid cairo surface state");
-
-            cr.set_source_rgba(1.0, 0.2, 0.2, 0.6);
-            cr.arc(0.04, 0.53, 0.02, 0.0, PI * 2.);
-            cr.arc(0.27, 0.65, 0.02, 0.0, PI * 2.);
-            cr.fill().expect("Invalid cairo surface state");
-            */
 
             if let Some(glyph) = obj.imp().glyph.get() {
-                println!("cairo drawing glyph {}", glyph.name);
-                glyph.draw(drar, cr, (0.2, 0.2), (0.8, 0.8));
+                //println!("cairo drawing glyph {}", glyph.name);
+                cr.translate(camera.0, camera.1);
+                glyph.draw(drar, cr, (10.0, 10.0), (500., 800.));
             } else {
-                println!("cairo drawing without glyph");
+                //println!("cairo drawing without glyph");
             }
 
-            Inhibit(false)
-        }
-        ));
+           Inhibit(false)
+        }));
         obj.add(&drawing_area);
+        obj.set_visible(true);
+        obj.set_expand(true);
+        obj.set_can_focus(true);
 
         self.drawing_area
             .set(drawing_area)
@@ -120,9 +165,10 @@ glib::wrapper! {
 }
 
 impl GlyphEditView {
-    pub fn new(glyph: Glyph) -> Self {
+    pub fn new(app: gtk::Application, glyph: Glyph) -> Self {
         let ret: Self = glib::Object::new(&[]).expect("Failed to create Main Window");
         ret.imp().glyph.set(glyph).unwrap();
+        ret.imp().app.set(app).unwrap();
         ret
     }
 }
